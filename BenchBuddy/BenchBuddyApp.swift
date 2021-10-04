e426d348089ba84d57f3465119dbe873ec120239
Christopher Dwyer <chris.dwyer.dev@gmail.com>
2021-10-03 23:57:23 -0400

    Initial Commit

---
+++ BenchBuddy/BenchBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenchBuddyApp.swift
+//  BenchBuddy
+//
+//  Created by Christopher Dwyer on 10/3/21.
+//
+
+import SwiftUI
+
+@main
+struct BenchBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
